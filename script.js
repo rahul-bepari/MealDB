177528a37b099ebe33123655a810f2110d041387
Rahul Bepari <rahulbepari35@gmail.com>
2026-08-21 17:13:40 +0600

    some issue fix

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function connect(){
     var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
 
     var displayArea = document.getElementById("displayArea");
-
     var statusMessage = document.getElementById("statusMessage");
-
     var showAllBtn = document.getElementById("showAllBtn");
 
+    displayArea.innerHTML = ""; 
+    statusMessage.innerHTML = "";
+    showAllBtn.classList.add("d-none");
     allMeals = [];
+    window.scrollTo(0, 0);
+
     fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -34,52 +37,33 @@ function connect(){
 }
 
 function showAll(){
-    show(allMeals.slice(5));
+    show(allMeals.slice(5)); 
     document.getElementById("showAllBtn").classList.add("d-none");
 }
 
 function show(meals){
 
     var displayArea = document.getElementById("displayArea");
+
     for (var i = 0; i < meals.length; i++){
 
         var newDiv = document.createElement("div");
         newDiv.classList.add("col-12", "col-sm-6", "col-lg-4", "col-xl-3");
 
-
-        newDiv.innerHTML =` <div class="card h-100 shadow-sm innerStyle">
-                            <img src="${meals[i].strMealThumb}" class="card-img-top" alt="${meals[i].strMeal}">
-                                <div class="card-body">
-                                <h5 class="card-title">${meals[i].strMeal}</h5>
-                                <p class="text-muted mb-2">Meal ID: ${meals[i].idMeal}</p>
-                                <p class="meal-instructions">${meals[i].strInstructions.substring(0, 150)}...</p>
-
-
-
-
-                                 </div>
-
-                            </div>
-        
+        newDiv.innerHTML = `
+            <div class="card h-100 shadow-sm innerStyle">
+                <img src="${meals[i].strMealThumb}" class="card-img-top" alt="${meals[i].strMeal}">
+                <div class="card-body">
+                    <p class="mb-1"><strong>Meal ID:</strong> ${meals[i].idMeal}</p>
+                    <p class="mb-1"><strong>Meal Name:</strong> ${meals[i].strMeal}</p>
+                    <p class="mb-2"><strong>Meal Title:</strong> ${meals[i].strMeal}</p>
+                    <p class="meal-instructions"><strong>Cooking Instructions:</strong> ${meals[i].strInstructions.substring(0, 150)}...</p>
+                </div>
+            </div>
         `;
 
         displayArea.appendChild(newDiv);
 
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
